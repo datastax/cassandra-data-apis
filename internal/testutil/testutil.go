a95f3fea91451c8839ee852b28d2deedd8fc9cbe
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2020-03-25 10:12:35 +0100

    CI: Add AppVeyor for integration tests and TravisCI for unit tests
    - Sample integration test
    - AppVeyor for Linux support with ccm
    - TravisCI for unit tests

---
+++ internal/testutil/testutil.go
@@ -0,0 +1,80 @@
+package testutil
+
+import (
+	"fmt"
+	"github.com/gocql/gocql"
+	"os"
+	"os/exec"
+)
+
+var started = false
+
+func startCassandra() {
+	if started {
+		return
+	}
+	started = true
+	fmt.Println("Starting Cassandra")
+	//ccmCmd := "ccm status || true"
+	ccmCmd := fmt.Sprintf("ccm create test -v %s -n 1 -s -b", cassandraVersion())
+	cmd := exec.Command("bash", "-c", ccmCmd)
+
+	output, err := cmd.CombinedOutput()
+	fmt.Println("Output", string(output))
+	if err != nil {
+		fmt.Println("Error", err)
+		panic(err)
+	}
+}
+
+func shutdownCassandra() {
+	fmt.Println("Shutting down cassandra")
+	cmd := exec.Command("bash", "-c", "ccm remove")
+	output, err := cmd.CombinedOutput()
+	fmt.Println("Output", string(output))
+	if err != nil {
+		fmt.Println("Error", err)
+		panic(err)
+	}
+}
+
+func IntegrationTestsEnabled() bool {
+	return os.Getenv("RUN_INTEGRATION_TESTS") == "1"
+}
+
+func cassandraVersion() string {
+	version := os.Getenv("CCM_VERSION")
+	if version == "" {
+		version = "3.11.6"
+	}
+	return version
+}
+
+func SetupIntegrationTestFixture() *gocql.Session {
+	if !IntegrationTestsEnabled() {
+		return nil
+	}
+
+	startCassandra()
+
+	cluster := gocql.NewCluster("127.0.0.1")
+
+	var (
+		session *gocql.Session
+		err     error
+	)
+
+	if session, err = cluster.CreateSession(); err != nil {
+		panic(err)
+	}
+
+	return session
+}
+
+func TearDownIntegrationTestFixture() {
+	if !IntegrationTestsEnabled() {
+		return
+	}
+
+	shutdownCassandra()
+}
