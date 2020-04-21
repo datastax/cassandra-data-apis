d771b8107d5fb98f032fffa967dd9a1916bd88bb
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2020-04-21 17:27:52 +0200

    Support scalar and collection types for DDL queries

--- graphql/table.go
+++ graphql/table.go
@@ -1,6 +1,7 @@
 package graphql
 
 import (
+	"errors"
 	"fmt"
 	"github.com/datastax/cassandra-data-apis/db"
 	"github.com/gocql/gocql"
@@ -8,41 +9,29 @@ import (
 	"github.com/mitchellh/mapstructure"
 )
 
-const (
-	typeInt = iota
-	typeVarchar
-	typeText
-	typeUUID
-	// ...
-)
-
-const (
-	kindUnknown = iota
-	kindPartition
-	kindClustering
-	kindRegular
-	kindStatic
-	kindCompact
-)
-
 type dataTypeValue struct {
-	Basic    int              `json:"basic"`
-	SubTypes []*dataTypeValue `json:"subTypes"`
+	Basic    gocql.Type    `json:"basic"`
+	TypeInfo *dataTypeInfo `json:"info"`
+}
+
+type dataTypeInfo struct {
+	Name     string          `json:"name"`
+	SubTypes []dataTypeValue `json:"subTypes"`
 }
 
 type columnValue struct {
-	Name string         `json:"name"`
-	Kind int            `json:"kind"`
-	Type *dataTypeValue `json:"type"`
+	Name string           `json:"name"`
+	Kind gocql.ColumnKind `json:"kind"`
+	Type *dataTypeValue   `json:"type"`
 }
 
 type clusteringInfo struct {
 	// mapstructure.Decode() calls don't work when embedding values
 	//columnValue  //embedded
-	Name  string         `json:"name"`
-	Kind  int            `json:"kind"`
-	Type  *dataTypeValue `json:"type"`
-	Order string         `json:"order"`
+	Name  string           `json:"name"`
+	Kind  gocql.ColumnKind `json:"kind"`
+	Type  *dataTypeValue   `json:"type"`
+	Order string           `json:"order"`
 }
 
 type tableValue struct {
@@ -53,19 +42,33 @@ type tableValue struct {
 var basicTypeEnum = graphql.NewEnum(graphql.EnumConfig{
 	Name: "BasicType",
 	Values: graphql.EnumValueConfigMap{
-		"INT": &graphql.EnumValueConfig{
-			Value: typeInt,
-		},
-		"VARCHAR": &graphql.EnumValueConfig{
-			Value: typeVarchar,
-		},
-		"TEXT": &graphql.EnumValueConfig{
-			Value: typeText,
-		},
-		"UUID": &graphql.EnumValueConfig{
-			Value: typeUUID,
-		},
-		// ...
+		"ASCII":     {Value: gocql.TypeAscii},
+		"VARCHAR":   {Value: gocql.TypeVarchar},
+		"TEXT":      {Value: gocql.TypeText},
+		"BOOLEAN":   {Value: gocql.TypeBoolean},
+		"FLOAT":     {Value: gocql.TypeFloat},
+		"DOUBLE":    {Value: gocql.TypeDouble},
+		"TINYINT":   {Value: gocql.TypeTinyInt},
+		"SMALLINT":  {Value: gocql.TypeSmallInt},
+		"INT":       {Value: gocql.TypeInt},
+		"BIGINT":    {Value: gocql.TypeBigInt},
+		"VARINT":    {Value: gocql.TypeVarint},
+		"DECIMAL":   {Value: gocql.TypeDecimal},
+		"COUNTER":   {Value: gocql.TypeCounter},
+		"UUID":      {Value: gocql.TypeUUID},
+		"TIMEUUID":  {Value: gocql.TypeTimeUUID},
+		"TIME":      {Value: gocql.TypeTime},
+		"DATE":      {Value: gocql.TypeDate},
+		"DURATION":  {Value: gocql.TypeDuration},
+		"TIMESTAMP": {Value: gocql.TypeTimestamp},
+		"BLOB":      {Value: gocql.TypeBlob},
+		"INET":      {Value: gocql.TypeInet},
+		"LIST":      {Value: gocql.TypeList},
+		"SET":       {Value: gocql.TypeSet},
+		"MAP":       {Value: gocql.TypeMap},
+		"TUPLE":     {Value: gocql.TypeTuple},
+		"UDT":       {Value: gocql.TypeUDT},
+		"CUSTOM":    {Value: gocql.TypeCustom},
 	},
 })
 
@@ -75,13 +78,20 @@ func buildDataType() *graphql.Object {
 	dataType := graphql.NewObject(graphql.ObjectConfig{
 		Name: "DataType",
 		Fields: graphql.Fields{
-			"basic": &graphql.Field{
-				Type: graphql.NewNonNull(basicTypeEnum),
-			},
+			"basic": {Type: graphql.NewNonNull(basicTypeEnum)},
 		},
 	})
-	dataType.AddFieldConfig("subTypes", &graphql.Field{
-		Type: graphql.NewList(dataType),
+
+	info := graphql.NewObject(graphql.ObjectConfig{
+		Name: "DataTypeInfo",
+		Fields: graphql.Fields{
+			"name":     {Type: graphql.String},
+			"subTypes": {Type: graphql.NewList(dataType)},
+		},
+	})
+
+	dataType.AddFieldConfig("info", &graphql.Field{
+		Type: info,
 	})
 	return dataType
 }
@@ -89,39 +99,21 @@ func buildDataType() *graphql.Object {
 var columnKindEnum = graphql.NewEnum(graphql.EnumConfig{
 	Name: "ColumnKind",
 	Values: graphql.EnumValueConfigMap{
-		"UNKNOWN": &graphql.EnumValueConfig{
-			Value: kindUnknown,
-		},
-		"PARTITION": &graphql.EnumValueConfig{
-			Value: kindPartition,
-		},
-		"CLUSTERING": &graphql.EnumValueConfig{
-			Value: kindClustering,
-		},
-		"REGULAR": &graphql.EnumValueConfig{
-			Value: kindRegular,
-		},
-		"STATIC": &graphql.EnumValueConfig{
-			Value: kindStatic,
-		},
-		"COMPACT": &graphql.EnumValueConfig{
-			Value: kindCompact,
-		},
+		"UNKNOWN":    {Value: gocql.ColumnUnkownKind},
+		"PARTITION":  {Value: gocql.ColumnPartitionKey},
+		"CLUSTERING": {Value: gocql.ColumnClusteringKey},
+		"REGULAR":    {Value: gocql.ColumnRegular},
+		"STATIC":     {Value: gocql.ColumnStatic},
+		"COMPACT":    {Value: gocql.ColumnCompact},
 	},
 })
 
 var columnType = graphql.NewObject(graphql.ObjectConfig{
 	Name: "Column",
 	Fields: graphql.Fields{
-		"name": &graphql.Field{
-			Type: graphql.NewNonNull(graphql.String),
-		},
-		"kind": &graphql.Field{
-			Type: graphql.NewNonNull(columnKindEnum),
-		},
-		"type": &graphql.Field{
-			Type: graphql.NewNonNull(dataType),
-		},
+		"name": {Type: graphql.NewNonNull(graphql.String)},
+		"kind": {Type: graphql.NewNonNull(columnKindEnum)},
+		"type": {Type: graphql.NewNonNull(dataType)},
 	},
 })
 
@@ -131,13 +123,20 @@ func buildDataTypeInput() *graphql.InputObject {
 	dataType := graphql.NewInputObject(graphql.InputObjectConfig{
 		Name: "DataTypeInput",
 		Fields: graphql.InputObjectConfigFieldMap{
-			"basic": &graphql.InputObjectFieldConfig{
-				Type: graphql.NewNonNull(basicTypeEnum),
-			},
+			"basic": {Type: graphql.NewNonNull(basicTypeEnum)},
 		},
 	})
-	dataType.AddFieldConfig("subTypes", &graphql.InputObjectFieldConfig{
-		Type: graphql.NewList(dataType),
+
+	info := graphql.NewInputObject(graphql.InputObjectConfig{
+		Name: "DataTypeInfoInput",
+		Fields: graphql.InputObjectConfigFieldMap{
+			"name":     {Type: graphql.String},
+			"subTypes": {Type: graphql.NewList(dataType)},
+		},
+	})
+
+	dataType.AddFieldConfig("info", &graphql.InputObjectFieldConfig{
+		Type: info,
 	})
 	return dataType
 }
@@ -145,60 +144,60 @@ func buildDataTypeInput() *graphql.InputObject {
 var columnInput = graphql.NewInputObject(graphql.InputObjectConfig{
 	Name: "ColumnInput",
 	Fields: graphql.InputObjectConfigFieldMap{
-		"name": &graphql.InputObjectFieldConfig{
-			Type: graphql.NewNonNull(graphql.String),
-		},
-		"type": &graphql.InputObjectFieldConfig{
-			Type: graphql.NewNonNull(dataTypeInput),
-		},
+		"name": {Type: graphql.NewNonNull(graphql.String)},
+		"type": {Type: graphql.NewNonNull(dataTypeInput)},
 	},
 })
 
 var clusteringKeyInput = graphql.NewInputObject(graphql.InputObjectConfig{
 	Name: "ClusteringKeyInput",
 	Fields: graphql.InputObjectConfigFieldMap{
-		"name": &graphql.InputObjectFieldConfig{
-			Type: graphql.NewNonNull(graphql.String),
-		},
-		"type": &graphql.InputObjectFieldConfig{
-			Type: graphql.NewNonNull(dataTypeInput),
-		},
-		"order": &graphql.InputObjectFieldConfig{
-			Type: graphql.String,
-		},
+		"name":  {Type: graphql.NewNonNull(graphql.String)},
+		"type":  {Type: graphql.NewNonNull(dataTypeInput)},
+		"order": {Type: graphql.String},
 	},
 })
 
 var tableType = graphql.NewObject(graphql.ObjectConfig{
 	Name: "Table",
 	Fields: graphql.Fields{
-		"name": &graphql.Field{
-			Type: graphql.NewNonNull(graphql.String),
-		},
-		"columns": &graphql.Field{
-			Type: graphql.NewList(columnType),
-		},
+		"name":    {Type: graphql.NewNonNull(graphql.String)},
+		"columns": {Type: graphql.NewList(columnType)},
 	},
 })
 
-func getTable(keyspace *gocql.KeyspaceMetadata, args map[string]interface{}) (interface{}, error) {
-	name := args["name"].(string)
-	table := keyspace.Tables[name]
-	if table == nil {
-		return nil, fmt.Errorf("unable to find table '%s'", name)
-	}
-	return &tableValue{
-		Name:    name,
-		Columns: toColumnValues(table.Columns),
-	}, nil
-}
+func getTables(keyspace *gocql.KeyspaceMetadata, args map[string]interface{}) (interface{}, error) {
+	if args["name"] != nil {
+		// Filter by name
+		name := args["name"].(string)
+		table := keyspace.Tables[name]
+		if table == nil {
+			return nil, fmt.Errorf("unable to find table '%s'", name)
+		}
 
-func getTables(keyspace *gocql.KeyspaceMetadata) (interface{}, error) {
+		columns, err := toColumnValues(table.Columns)
+		if err != nil {
+			return nil, err
+		}
+
+		return []*tableValue{
+			{
+				Name:    table.Name,
+				Columns: columns,
+			},
+		}, nil
+	}
+
 	tableValues := make([]*tableValue, 0)
 	for _, table := range keyspace.Tables {
+		columns, err := toColumnValues(table.Columns)
+		if err != nil {
+			return nil, err
+		}
+
 		tableValues = append(tableValues, &tableValue{
 			Name:    table.Name,
-			Columns: toColumnValues(table.Columns),
+			Columns: columns,
 		})
 	}
 	return tableValues, nil
@@ -212,11 +211,17 @@ func decodeColumns(columns []interface{}) ([]*gocql.ColumnMetadata, error) {
 			return nil, err
 		}
 
+		columnType, err := toDbColumnType(value.Type)
+
+		if err != nil {
+			return nil, err
+		}
+
 		// Adapt from GraphQL column to gocql column
 		cqlColumn := &gocql.ColumnMetadata{
 			Name: value.Name,
-			Kind: toDbColumnKind(value.Kind),
-			Type: toDbColumnType(value.Type),
+			Kind: value.Kind,
+			Type: columnType,
 		}
 
 		columnValues = append(columnValues, cqlColumn)
@@ -232,11 +237,17 @@ func decodeClusteringInfo(columns []interface{}) ([]*gocql.ColumnMetadata, error
 			return nil, err
 		}
 
+		columnType, err := toDbColumnType(value.Type)
+
+		if err != nil {
+			return nil, err
+		}
+
 		// Adapt from GraphQL column to gocql column
 		cqlColumn := &gocql.ColumnMetadata{
 			Name: value.Name,
-			Kind: toDbColumnKind(value.Kind),
-			Type: toDbColumnType(value.Type),
+			Kind: value.Kind,
+			Type: columnType,
 			//TODO: Use enums
 			ClusteringOrder: value.Order,
 		}
@@ -350,90 +361,110 @@ func (sg *SchemaGenerator) dropTable(params graphql.ResolveParams) (interface{},
 		Table:    tableName}, db.NewQueryOptions().WithUserOrRole(userOrRole))
 }
 
-func toColumnKind(kind gocql.ColumnKind) int {
-	switch kind {
-	case gocql.ColumnPartitionKey:
-		return kindPartition
-	case gocql.ColumnClusteringKey:
-		return kindClustering
-	case gocql.ColumnRegular:
-		return kindRegular
-	case gocql.ColumnStatic:
-		return kindStatic
-	case gocql.ColumnCompact:
-		return kindCompact
-	default:
-		return kindUnknown
-	}
-}
-
-func toDbColumnKind(kind int) gocql.ColumnKind {
-	switch kind {
-	case kindPartition:
-		return gocql.ColumnPartitionKey
-	case kindClustering:
-		return gocql.ColumnClusteringKey
-	case kindRegular:
-		return gocql.ColumnRegular
-	case kindStatic:
-		return gocql.ColumnStatic
-	case kindCompact:
-		return gocql.ColumnCompact
-	default:
-		return kindUnknown
-	}
-}
-
-func toColumnType(info gocql.TypeInfo) *dataTypeValue {
+func toColumnType(info gocql.TypeInfo) (*dataTypeValue, error) {
+	var subTypeInfo *dataTypeInfo = nil
 	switch info.Type() {
-	case gocql.TypeInt:
-		return &dataTypeValue{
-			Basic:    typeInt,
-			SubTypes: nil,
+	case gocql.TypeList, gocql.TypeSet:
+		collectionInfo := info.(gocql.CollectionType)
+		subType, err := toColumnType(collectionInfo.Elem)
+
+		if err != nil {
+			return nil, err
 		}
-	case gocql.TypeVarchar:
-		return &dataTypeValue{
-			Basic:    typeVarchar,
-			SubTypes: nil,
+
+		subTypeInfo = &dataTypeInfo{
+			SubTypes: []dataTypeValue{*subType},
 		}
-	case gocql.TypeText:
-		return &dataTypeValue{
-			Basic:    typeText,
-			SubTypes: nil,
+	case gocql.TypeMap:
+		collectionInfo := info.(gocql.CollectionType)
+
+		keyType, err := toColumnType(collectionInfo.Key)
+		if err != nil {
+			return nil, err
 		}
-	case gocql.TypeUUID:
-		return &dataTypeValue{
-			Basic:    typeUUID,
-			SubTypes: nil,
+
+		valueType, err := toColumnType(collectionInfo.Elem)
+		if err != nil {
+			return nil, err
 		}
-		// ...
+
+		subTypeInfo = &dataTypeInfo{
+			SubTypes: []dataTypeValue{*keyType, *valueType},
+		}
+	case gocql.TypeCustom:
+		subTypeInfo = &dataTypeInfo{
+			Name: info.Custom(),
+		}
+	case gocql.TypeUDT, gocql.TypeTuple:
+		return nil, errors.New("Not yet supported")
 	}
-	return nil
+
+	return &dataTypeValue{
+		Basic:    info.Type(),
+		TypeInfo: subTypeInfo,
+	}, nil
 }
 
-func toDbColumnType(info *dataTypeValue) gocql.TypeInfo {
+func toDbColumnType(info *dataTypeValue) (gocql.TypeInfo, error) {
 	switch info.Basic {
-	case typeInt:
-		return gocql.NewNativeType(0, gocql.TypeInt, "")
-	case typeVarchar:
-		return gocql.NewNativeType(0, gocql.TypeVarchar, "")
-	case typeText:
-		return gocql.NewNativeType(0, gocql.TypeText, "")
-	case typeUUID:
-		return gocql.NewNativeType(0, gocql.TypeUUID, "")
-	}
+	case gocql.TypeList, gocql.TypeSet:
+		if info.TypeInfo == nil && len(info.TypeInfo.SubTypes) != 1 {
+			return nil, errors.New("you must provide one sub type for list and set data types")
+		}
 
-	return nil
+		subType, err := toDbColumnType(&info.TypeInfo.SubTypes[0])
+		if err != nil {
+			return nil, err
+		}
+
+		return gocql.CollectionType{
+			NativeType: gocql.NewNativeType(0, info.Basic, ""),
+			Key:        nil,
+			Elem:       subType,
+		}, nil
+	case gocql.TypeMap:
+		if info.TypeInfo == nil && len(info.TypeInfo.SubTypes) != 2 {
+			return nil, errors.New("you must provide the key and value sub type for map data types")
+		}
+
+		keyType, err := toDbColumnType(&info.TypeInfo.SubTypes[0])
+		if err != nil {
+			return nil, err
+		}
+
+		valueType, err := toDbColumnType(&info.TypeInfo.SubTypes[0])
+		if err != nil {
+			return nil, err
+		}
+
+		return gocql.CollectionType{
+			NativeType: gocql.NewNativeType(0, info.Basic, ""),
+			Key:        keyType,
+			Elem:       valueType,
+		}, nil
+	case gocql.TypeCustom:
+		return gocql.NewNativeType(0, info.Basic, info.TypeInfo.Name), nil
+	case gocql.TypeUDT, gocql.TypeTuple:
+		return nil, errors.New("udts and tuples are not supported yet")
+	default:
+		return gocql.NewNativeType(0, info.Basic, ""), nil
+	}
 }
 
-func toColumnValues(columns map[string]*gocql.ColumnMetadata) []*columnValue {
+func toColumnValues(columns map[string]*gocql.ColumnMetadata) ([]*columnValue, error) {
 	columnValues := make([]*columnValue, 0)
 	for _, column := range columns {
+		columnType, err := toColumnType(column.Type)
+
+		if err != nil {
+			return nil, err
+		}
+
 		columnValues = append(columnValues, &columnValue{
 			Name: column.Name,
-			Kind: toColumnKind(column.Kind),
-			Type: toColumnType(column.Type),
+			Kind: column.Kind,
+			Type: columnType,
 		})
 	}
-	return columnValues
+	return columnValues, nil
 }
