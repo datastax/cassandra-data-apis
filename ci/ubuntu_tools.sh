382021e90e2ad1235328cc92a5023860d3b436ca
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2020-04-27 17:31:43 +0200

    Add simulacron to appveyor

--- ci/ubuntu_tools.sh
+++ ci/ubuntu_tools.sh
@@ -10,3 +10,13 @@ popd
 ccm status || true
 
 export CCM_PATH="$(pwd)/ccm"
+export SIMULACRON_PATH="$(pwd)/simulacron/simulacron.jar"
+
+if [ ! -f "$SIMULACRON_PATH" ]; then
+    mkdir simulacron
+    pushd simulacron
+    wget https://github.com/datastax/simulacron/releases/download/0.9.0/simulacron-standalone-0.9.0.jar
+    chmod uog+rw simulacron-standalone-0.9.0.jar
+    ln -s simulacron-standalone-0.9.0.jar simulacron.jar
+    popd
+fi
