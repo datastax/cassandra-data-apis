a323b51dde57d3088bdd7a17a0f95a2143bb6f63
Roman Chernobelskiy <1326903+rchernobelskiy@users.noreply.github.com>
2020-06-17 15:27:52 -0400

    Added patch for a couple edge cases

--- go.mod
+++ go.mod
@@ -15,7 +15,7 @@ require (
 	github.com/onsi/ginkgo v1.12.0
 	github.com/onsi/gomega v1.9.0
 	github.com/pelletier/go-toml v1.7.0 // indirect
-	github.com/sergi/go-diff v1.1.0
+	github.com/sergi/go-diff v1.1.0 // indirect
 	github.com/spf13/afero v1.2.2 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/cobra v0.0.7
@@ -24,13 +24,10 @@ require (
 	github.com/spf13/viper v1.6.2
 	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/stretchr/testify v1.5.1
-	go.uber.org/atomic v1.6.0
 	go.uber.org/zap v1.14.1
 	golang.org/x/sys v0.0.0-20200331124033-c3d80250170d // indirect
-	golang.org/x/text v0.3.2 // indirect
 	gopkg.in/inf.v0 v0.9.1
 	gopkg.in/ini.v1 v1.55.0 // indirect
-	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
 
 replace github.com/graphql-go/graphql => github.com/riptano/graphql-go v0.7.9-null
--- graphql/keyspace_schema.go
+++ graphql/keyspace_schema.go
@@ -2,6 +2,8 @@ package graphql
 
 import (
 	"fmt"
+	"regexp"
+
 	"github.com/datastax/cassandra-data-apis/config"
 	"github.com/datastax/cassandra-data-apis/db"
 	"github.com/datastax/cassandra-data-apis/types"
@@ -94,6 +96,8 @@ var serialConsistencyEnum = graphql.NewEnum(graphql.EnumConfig{
 		"LOCAL_SERIAL": {Value: gocql.LocalSerial},
 	},
 })
+
+var validName = regexp.MustCompile(`^[_a-zA-Z][_a-zA-Z0-9]*$`)
 
 func (s *KeyspaceGraphQLSchema) buildType(typeInfo gocql.TypeInfo, isInput bool) (graphql.Output, error) {
 	switch typeInfo.Type() {
@@ -272,6 +276,10 @@ func (s *KeyspaceGraphQLSchema) buildTableTypes(keyspace *gocql.KeyspaceMetadata
 		for name, column := range table.Columns {
 			var fieldType graphql.Output
 			var inputFieldType graphql.Output
+			if !validName.MatchString(table.Name) || validName.MatchString(name) {
+				err = fmt.Errorf("Table or column didn't match regex %s", validName.String())
+				break
+			}
 			fieldName := s.naming.ToGraphQLField(table.Name, name)
 			fieldType, err = s.buildType(column.Type, false)
 			if err != nil {
@@ -300,6 +308,10 @@ func (s *KeyspaceGraphQLSchema) buildTableTypes(keyspace *gocql.KeyspaceMetadata
 					Type: t,
 				}
 			}
+		}
+
+		if len(inputOperatorFields) == 0 || len(inputFields) == 0 || len(fields) == 0 {
+			err = fmt.Errorf("Value, scalar, or input array empty - perhaps a table with one unfilterable col?")
 		}
 
 		if err != nil {
