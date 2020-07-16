47b05d3c21671b537f225459ec09752c6117da27
Michael Penick <mpenick@users.noreply.github.com>
2020-07-16 10:34:12 -0400

    Remove schema version check from schema updater
    There have been spurious instances where the graphql schema wasn't
    updated and the mostly likely explanation is a problem with the DB
    schema version not being properly updated.

--- graphql/updater_test.go
+++ graphql/updater_test.go
@@ -15,10 +15,6 @@ func TestSchemaUpdater_Update(t *testing.T) {
 	sessionMock := db.NewSessionMock()
 	schemaGen := NewSchemaGenerator(db.NewDbWithSession(sessionMock), config.NewConfigMock().Default())
 
-	sessionMock.
-		SetSchemaVersion("a78bc282-aff7-4c2a-8f23-4ce3584adbb0").
-		Twice() // Called by `NewUpdater()` and the first `updater.update()`
-
 	keyspace := "store"
 
 	// Initial schema
@@ -42,11 +38,6 @@ func TestSchemaUpdater_Update(t *testing.T) {
 			"newTable1": db.BooksColumnsMock,
 		})).Once()
 
-	updater.update() // Schema version is set
-	// No change in the schema version, the updater will not read the new table
-	assert.NotContains(t, updater.Schema(keyspace).QueryType().Fields(), "newTable1")
-
-	sessionMock.SetSchemaVersion("2ca627b7-f869-4f0c-b995-142f903a0367")
-	updater.update() // Schema version changed
+	updater.update()
 	assert.Contains(t, updater.Schema(keyspace).QueryType().Fields(), "newTable1")
 }
